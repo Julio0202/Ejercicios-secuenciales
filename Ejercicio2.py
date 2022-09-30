a4cce9da39cd72538a87bf89c83cf5ef5ac7d6ed
Julio <al_ychen@iessierradelasvillas.org>
2022-09-30 18:18:23 +0200

    Ejercicios de python (los 5 primers)

---
+++ Ejercicio2.py
@@ -0,0 +1,7 @@
+base = 0
+altura = 0
+base = (int)(input("Dime lo que mide la base \n"))
+print("Ahora dime lo que mide la altura")
+altura = (int)(input())
+print("El perimetro del rectangulo es", base*2+altura*2)
+print("El área del rectangulo es", base*altura)
---
+++ Ejercicio5.py
@@ -0,0 +1,3 @@
+print("Dime los grados en Fahrenheit")
+gradofah = int(input())
+print("La conversión de F a Celsius es de",(gradofah-32)*5/9, "Celsius")
